1ce2e7102c6e4651e2037042ebf7391894d751fa
Julián Scialabba <julian.scialabba@bureapr.com>
2020-06-13 10:15:36 -0300

    Updates

---
+++ precache-manifest.03912c55ee67ec0e13a9088ae18ff4b1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "244f0c6bb779d2f8a1472b8d596657e1",
+    "url": "/css-flexbox-simulator/index.html"
+  },
+  {
+    "revision": "1ca8d327f432db29677c",
+    "url": "/css-flexbox-simulator/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "117829484d6a32483346",
+    "url": "/css-flexbox-simulator/static/js/2.84d34ccf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/css-flexbox-simulator/static/js/2.84d34ccf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ca8d327f432db29677c",
+    "url": "/css-flexbox-simulator/static/js/main.f1590eee.chunk.js"
+  },
+  {
+    "revision": "af9e9df36542ed60b611",
+    "url": "/css-flexbox-simulator/static/js/runtime-main.6528006f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/css-flexbox-simulator/static/media/logo.5d5d9eef.svg"
+  }
+]);
