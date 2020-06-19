754d79b1758018fb1048cf59fd2137db6a0786da
Julián Scialabba <julian.scialabba@bureapr.com>
2020-06-19 17:19:15 -0300

    Add project github link

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
-import { HeaderContainer, HeaderTitle } from './styled';
+import { HeaderContainer, HeaderTitle, HeaderLinkGithub } from './styled';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
 
 class Header extends Component {
   render() {
     return (<HeaderContainer>
        <HeaderTitle>
-          CSS Flexbox Simulator
+          CSS Flexbox Simulator 
        </HeaderTitle>
+       <HeaderLinkGithub href="https://github.com/juliansci/css-flexbox-simulator" target="_blank"> 
+         View on Github <FontAwesomeIcon icon={faGithub} />
+        </HeaderLinkGithub>
     </HeaderContainer>);
   }
 }
--- src/Components/Header/styled.js
+++ src/Components/Header/styled.js
@@ -1,13 +1,23 @@
 import styled from 'styled-components';
 
-export const HeaderContainer = styled.div`  
+export const HeaderContainer = styled.div`
    height: 100%;
    width: 100%;
    background-color: #0C090A;;
    color: white;
+   display: flex;
+   justify-content: space-between;
 `
 
-export const HeaderTitle = styled.div`  
+export const HeaderTitle = styled.div`
    font-size: 27px;
    padding: 18px 40px;
+`
+export const HeaderLinkGithub = styled.a`
+   font-size: 13px;
+   margin-top: 29px;
+   margin-right: 34px;
+   cursor: pointer;
+   color: white;
+   text-decoration: none;
 `
