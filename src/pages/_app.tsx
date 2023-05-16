fd7bf8870c5d3d355190995f27ef90c2016f7a37
Juliansci <julian.scialabba@gmail.com>
2023-05-16 09:20:35 -0300

    Add analytics config

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,12 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { GoogleAnalytics } from "nextjs-google-analytics";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <GoogleAnalytics trackPageViews />
+      <Component {...pageProps} />;
+    </>
+  );
 }
